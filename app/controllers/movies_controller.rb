83ae97f9a601cdcd6495eb71045324ca3a661a3c
VincentLangat033 <kimutaiketer033@gmail.com>
2023-04-23 13:10:51 +0300

    Update the movies_controller.rb file to include the index and create actions:

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,23 @@
 class MoviesController < ApplicationController
+    def index
+        @movies = Movie.all
+        render json: @movies
+      end
+    
+      def create
+        movies_params[:movies].each do |movie|
+          Movie.find_or_create_by(tmdb_id: movie["id"]) do |m|
+            m.title = movie["title"]
+            m.overview = movie["overview"]
+            m.release_date = movie["release_date"]
+          end
+        end
+        head :ok
+      end
+    
+      private
+    
+      def movies_params
+        params.permit(movies: [:id, :title, :overview, :release_date])
+      end
 end
