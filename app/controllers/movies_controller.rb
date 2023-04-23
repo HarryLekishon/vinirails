beeec04dec6825f77f5caf1eb119286d85dbc607
VincentLangat033 <kimutaiketer033@gmail.com>
2023-04-23 16:18:49 +0300

    verify authenticity token

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+    skip_before_action :verify_authenticity_token
     skip_before_action :authorized!, only: [:create, :index]
     def index
         @movies = Movie.all
@@ -6,6 +7,17 @@ class MoviesController < ApplicationController
       end
     
       def create
+        # movies_params = params.require(:movies)
+        # movies_params.each do |movie_params|
+        #   movie = Movie.new(movie_params.permit(:title, :overview, :release_date))
+        #   if movie.save
+        #     # Successfully saved the movie to the database
+        #     render json: { message: 'Movies were successfully saved to the database' }, status: :created
+        #   else
+        #     # Error saving the movie to the database
+        #     render json: { error: 'Failed to save movies to the database' }, status: :unprocessable_entity
+        #   end
+        # end
         movies_params[:movies].each do |movie|
           Movie.find_or_create_by(tmdb_id: movie["id"]) do |m|
             m.title = movie["title"]
